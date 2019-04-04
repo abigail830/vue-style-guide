63106f0bc5c27945fa4ffd5c9b9368fa863d74b0
SaraQian <abigail830@163.com>
2019-04-04 19:14:05 +0800

    update data assign comment

--- src/components/QuiButton.vue
+++ src/components/QuiButton.vue
@@ -6,6 +6,13 @@
 
 <script>
 export default {
+  // Method 1 to assign value of msg
+  // data:function(){
+  //   return {
+  //     msg:'下载'
+  //   }
+  // }
+  // Method 2 to open props of msg, so that value could assign outside
   props: {
     msg: {
       default: '下载'
