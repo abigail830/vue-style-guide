49ce0eb076d45f31a2efbcfb9ce3a0dffec47ea0
SaraQian <abigail830@163.com>
2019-04-04 16:55:55 +0800

    update button

--- src/pages/PageQuiButton.vue
+++ src/pages/PageQuiButton.vue
@@ -1,7 +1,57 @@
 <template>
   <div id="pageQuiButton">
     <bootstrapNav/>
-    <qui-btn msg="确定" class="small"></qui-btn>
+    <div class="btn-block">
+      <h4>Here is the BootstrapVue pre-defined Button</h4>
+      <div class='btn-group'>
+        <b-button variant="primary" class='b-button'>Primary Button</b-button>
+        <b-button variant="danger" class='b-button'>Danger Button</b-button>
+        <b-button variant="success" class='b-button'>Success Button</b-button>
+        <b-button class='b-button'>Common Button</b-button>
+        <b-button variant="outline-primary" class='b-button'>Outline-Primary Button</b-button>
+      </div>
+    </div>
+    <div class="btn-block">
+      <h4>Here is the Bootstrp pre-defined Button</h4>
+      <div class="btn-group">
+        <button type="button" class="b-button btn btn-primary">Primary</button>
+        <button type="button" class="b-button btn btn-secondary">Secondary</button>
+        <button type="button" class="b-button btn btn-success">Success</button>
+        <button type="button" class="b-button btn btn-danger">Danger</button>
+        <button type="button" class="b-button btn btn-warning">Warning</button>
+        <button type="button" class="b-button btn btn-info">Info</button>
+        <button type="button" class="b-button btn btn-light">Light</button>
+        <button type="button" class="b-button btn btn-dark">Dark</button>
+        <button type="button" class="b-button btn btn-link">Link</button>
+      </div>
+      <br/>
+      <div class="btn-group">
+        <button type="button" class="b-button btn btn-outline-primary">Primary</button>
+        <button type="button" class="b-button btn btn-outline-secondary">Secondary</button>
+        <button type="button" class="b-button btn btn-outline-success">Success</button>
+        <button type="button" class="b-button btn btn-outline-danger">Danger</button>
+        <button type="button" class="b-button btn btn-outline-warning">Warning</button>
+        <button type="button" class="b-button btn btn-outline-info">Info</button>
+        <button type="button" class="b-button btn btn-outline-light">Light</button>
+        <button type="button" class="b-button btn btn-outline-dark">Dark</button>
+      </div>
+      <br/>
+      <div class="btn-group">
+        <button type="button" class="b-button btn btn-primary btn-lg">Large button</button>
+        <button type="button" class="b-button btn btn-primary btn-sm">Small button</button>
+        <a href="#" class="b-button btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+        <button type="button" class="b-button btn btn-lg btn-primary" disabled>Disabled button</button>
+        <input class="b-button btn btn-primary" type="button" value="Input">
+        <input class="b-button btn btn-primary" type="submit" value="Submit">
+        <input class="b-button btn btn-primary" type="reset" value="Reset">
+      </div>
+    </div>
+    <div class="btn-block">
+      <h4>Here is the Self pre-defined Button</h4>
+      <div class='btn-group'>
+        <qui-btn msg="Qui-Btn" class="small"></qui-btn>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,3 +71,16 @@ export default {
   }
 }
 </script>
+<style>
+.btn-block{
+  margin-top: 20px;
+  margin-bottom: 40px;
+
+}
+.b-button{
+  margin: 0px 5px;
+}
+.btn-group{
+  margin-top: 15px;
+}
+</style>
